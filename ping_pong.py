fb8d7587c425b2b140b7663b50bcd5071e917a73
rohitw3code <rohitcode005@gmail.com>
2022-12-30 21:21:32 +0530

    updated

--- ping_pong.py
+++ ping_pong.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 
 capture = cv2.VideoCapture(0)
-frameWidth = 900
+frameWidth = 500
 frameHeight = 500
 capture.set(3, frameWidth)
 capture.set(4, frameHeight)
@@ -61,7 +61,7 @@ class Ball():
                             0.5, color, 1, cv2.LINE_AA)
 
 
-ball = Ball(radius=15, color=(0, 255, 0), step=18, startX=500)
+ball = Ball(radius=15, color=(100, 255, 100), step=18, startX=500)
 
 
 class Wall():
@@ -83,7 +83,7 @@ class Wall():
 detector = HandDetector(maxHands=2, detectionCon=0.8)
 
 wall1 = Wall(posx=100, posy=10, width=35, height=100)
-wall2 = Wall(posx=700, posy=10, width=35, height=100)
+wall2 = Wall(posx=500, posy=10, width=35, height=100)
 
 hand_cy1 = 10
 hand_cy2 = 10
@@ -93,7 +93,13 @@ while True:
     success, img = capture.read()
     img = cv2.flip(img, 1)
     hand, image = detector.findHands(img, draw=True)
-    img = np.zeros((frameHeight, frameWidth, 3), dtype='uint8')
+    # zimg = np.ones((frameHeight, frameWidth, 3), dtype=np.float)/10
+    # img = img*zimg
+    # overlay = img.copy()
+    # x, y, w, h = 0,0 , 1000, 10000
+    # cv2.rectangle(overlay, (x, y), (x + w, y + h), (255, 250,250), -1)
+    # alpha = 0.4  # Transparency factor.
+    # img = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
 
     if hand:
         if len(hand) == 2:
@@ -106,6 +112,13 @@ while True:
             cv2.circle(img, (hand_cx1, hand_cy1), 5, (255, 0, 100), cv2.FILLED)
             cv2.circle(img, (hand_cx2, hand_cy2), 5, (100,0, 255), cv2.FILLED)
 
+        if len(hand) == 1:
+            hand_cx1 = hand[0]['lmList'][8][0]
+            hand_cy1 = hand[0]['lmList'][8][1]
+
+            cv2.circle(img, (hand_cx1, hand_cy1), 5, (255, 0, 100), cv2.FILLED)
+
+
     ball.img = img
     ball.createBall(img)
     ball.bounceBack()
@@ -113,11 +126,11 @@ while True:
     wall2.createWall(img, hand_cy2)
     ball.wallCollision(wall1, wall2)
 
-    ball.drawText("Player 1 ",(10,20))
-    ball.drawText("Player 2",(750,20))
+    ball.drawText("Player 1 ",(10,20),color=(0,0,0))
+    ball.drawText("Player 2",(520,20),color=(0,0,0))
 
-    ball.drawText(f"Score : {ball.score1}",(10,40))
-    ball.drawText(f"Score : {ball.score2}",(750,40))
+    ball.drawText(f"Score : {ball.score1}",(10,40),color=(0,0,0))
+    ball.drawText(f"Score : {ball.score2}",(520,40),color=(0,0,0))
 
     cv2.imshow("Ping Pong", img)
 
